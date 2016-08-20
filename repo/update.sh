3c5eaa7a9bdf7f2ec7a28cc280359b18e2bee8c3
KurdVision <kurdvision@hotmail.com>
2016-08-20 07:25:01 -0700

    Delete update.sh

--- repo/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm Packages{,.bz2}
-dpkg-scanpackages -m . > Packages
-bzip2 Packages
-ncftpput -u <username> -p <password> <ftp.address> repo/ CydiaIcon.png
-ncftpput -u <username> -p <password> <ftp.address> repo/ Release
-ncftpput -u <username> -p <password> <ftp.address> repo/Packages/ debs/*
-ncftpput -u <username> -p <password> <ftp.address> repo/ Packages.bz2
